1fb59d1027e33fdc362ba324149f318229812e86
Trini Truong <trinitruong001@gmail.com>
2020-03-01 01:47:55 -0600

    Explains for conditional statement and how to use it

---
+++ for.py
@@ -0,0 +1,12 @@
+# for conditional statement = for "object" in list, then print(xyz)
+# xyz is a variable to be defined as needed
+def main():
+    # Loop over a list
+    answers = ["super", "user", "use"]
+
+    for answer in answers:
+        print(answer)
+
+
+if __name__ == "__main__":
+    main()
