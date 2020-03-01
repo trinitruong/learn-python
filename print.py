448699d57e5e5ce7a41349c5da55a6c53e27b70f
Trini Truong <trinitruong001@gmail.com>
2020-03-01 01:36:22 -0600

    Explains print function

---
+++ print.py
@@ -0,0 +1,20 @@
+#to print verbatim use print() function
+def main():
+    # print a string to the console/terminal
+    #use single or double quotes inside parenthesis to create string
+    print("something")
+
+    # print a variable
+    #a is the variable, 321321 is value assigned to variable
+    a = 321321
+    print(a)
+
+    b = "afhgdsgds"
+    print(b)
+
+    c = 7.62863
+    print(c)
+
+
+if __name__ == "__main__":
+    main()
