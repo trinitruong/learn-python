20cbf99c41cb78d54009d4dbe169b89e54f957a0
Trini Truong <trinitruong001@gmail.com>
2020-03-02 00:32:44 -0600

    Define variables, int, int func, float, and float func

---
+++ types_variables.py
@@ -0,0 +1,12 @@
+# definitions of types and variables
+# integers = whole numbers
+# float = decimals
+
+# price = variable
+price = 12 * 1.39
+
+# will print decimal result of price variable
+print(float(price))
+
+# will print integer (whole number) result of price
+print(int(price))
