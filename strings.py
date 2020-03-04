52c38fe6ac9b028d0dae1cf79582d288d6b1faa7
Trini Truong <trinitruong001@gmail.com>
2020-03-04 16:12:25 -0600

    Explains basic string operations and slicing

--- strings.py
+++ strings.py
@@ -12,3 +12,17 @@
 place = "cancun"
 year = 2020
 print("We would love to go to %s in %s!" % (place, year))
+
+# basic string operations
+string = "hello world"
+print(len(string)) # len() will return the length (number of items) of string ; spaces are count as 1
+
+print(string.index("o")) # will return "o" in "hello world" as an integer
+
+print(string.count("l")) #counts how many "l" in the string
+
+# square brackets [] can slice a string
+print(string[2:5]) # will print letters/spaces that are indexed 2-5 ; spaces count when indexing
+# first number is inclusive and last number is exclusive
+print(string[2:-1]) # negative number = start count from the end
+print(string[::2]) # [start:stop:step] - last colon will skip over specified step to skip
